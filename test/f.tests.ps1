68b2d55cefb209459a2ca2a575130d58cd1be4c7
Ben Tucker <bjtucker@users.noreply.github.com>
2019-07-21 00:52:36 -0500

    Update f.tests.ps1

--- test/f.tests.ps1
+++ test/f.tests.ps1
@@ -1,1 +1,9 @@
-exit 0
+#$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+#$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+#. "$here\$sut"
+ 
+Describe “Get-HelloWorld" {
+    It "does something useful" {
+        $true | Should Be $false
+    }
+}
